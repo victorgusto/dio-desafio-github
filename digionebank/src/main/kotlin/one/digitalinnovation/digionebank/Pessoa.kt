b5a8cf637e62cee3bd9c3520828a1469aa6e5487
victorgusto <victorgusto@gmail.com>
2022-07-17 15:02:56 -0300

    Classes Funcionario e Pessoa
    Atualização para fazer a classe pessoa e funcionário abstratas

--- digionebank/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ digionebank/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,16 +1,7 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
-    var nome : String = "Victor"
-    var cpf : String = "123.456.789-10"
-    private set
+abstract class Pessoa (
+    val nome: String,
+    val cpf: String
+    )
 
-    constructor()
-
-    fun pessoaInfo() = "$nome e $cpf"
-}
-
-fun main(){
-    val victor = Pessoa()
-    println(victor.pessoaInfo())
-}
