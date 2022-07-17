81b3b4cba315c9f7178adfb42e5671118c05460f
victorgusto <victorgusto@gmail.com>
2022-07-17 17:15:48 -0300

    Novas operações
    Foram acrescentadas algumas novas operações que podem ser utilizadas em uma Array.

--- api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteOperacoes.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteOperacoes.kt
@@ -16,4 +16,17 @@ fun main() {
     println("\n----------------")
     salariosMaiorQue2500.forEach{println(it)}
 
+    println("\n----------------")
+    println(salarios.count { it in 2500.0..5000.0 })
+
+    println("\n----------------")
+    println(salarios.find { it == 2780.0 })
+    println(salarios.find { it == 500.0 })
+
+    println("\n----------------")
+    println(salarios.any { it == 2780.0 })
+    println(salarios.any { it == 500.0 })
+
+
+
 }
