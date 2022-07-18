040f3a897f757104b12a89ccc7695d28b91685ff
victorgusto <victorgusto@gmail.com>
2022-07-17 21:03:05 -0300

    Função Set
    Foi extraída a classe funcionario para o arquivo Funcionario separado e  criado um novo arquivo Set.

--- api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -24,15 +24,3 @@ fun main() {
 
 }
 
-data class Funcionario(
-    val nome: String,
-    val salario: Double,
-    val tipoContratacao: String
-){
-    override fun toString(): String =
-        """
-            Nome: $nome
-            Salário: $salario
-        """.trimIndent()
-
-}
---
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteSet.kt
@@ -0,0 +1,23 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val joao = Funcionario("Joao", 3600.0, "CLT")
+    val pedro = Funcionario("Pedro", 1000.0, "PJ")
+    val maria = Funcionario("Maria", 5000.0, "CLT")
+
+    val funcionarios1 = setOf(joao,pedro)
+    val funcionarios2 = setOf(maria)
+
+    val resultUnion = funcionarios1.union(funcionarios2)
+    resultUnion.forEach{ println(it)}
+
+    println("----------------------")
+    val funcionarios3 = setOf(joao, pedro, maria)
+    val resultSubtract = funcionarios3.subtract(funcionarios2)
+    resultSubtract.forEach{ println(it)}
+
+    println("----------------------")
+    val resultIntersect = funcionarios3.intersect(funcionarios2)
+    resultIntersect.forEach{ println(it)}
+
+}
